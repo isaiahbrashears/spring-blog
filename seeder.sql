070a9d2bafd1aa7a9cbbbfbed0b4774a6ac56a88
Isaiah Brashears <isaiah.brashears13@gmail.com>
2019-11-08 15:35:50 -0600

    add tags

--- seeder.sql
+++ seeder.sql
@@ -17,8 +17,26 @@ insert into posts (user_id, title, body) values (1,'Sugar Curtain, The (El teló
 insert into posts (user_id, title, body) values (1,'Golem', 'Customizable static access');
 
 
-
-
 insert into images (title, url, post_id) values ('cat 1', 'https://pmcvariety.files.wordpress.com/2019/08/garfield-e1565072358875.jpg?w=1000&h=563&crop=1' ,1);
 insert into images (title, url, post_id) values ('cat 2', 'https://cdn2us.denofgeek.com/sites/denofgeekus/files/styles/main_wide/public/2019/08/garfield-and-friends.jpg?itok=NGvVnLsF',1);
 insert into images (title, url, post_id) values ('cat 3', 'https://i.kym-cdn.com/entries/icons/original/000/029/052/creepy_garfield_cover.jpg',1);
+
+
+INSERT INTO tags (name) VALUES ('Funny');
+INSERT INTO tags (name) VALUES ('Silly');
+INSERT INTO tags (name) VALUES ('Serious');
+INSERT INTO tags (name) VALUES ('Dog Stuff');
+
+insert into post_tag (post_id, tag_id) values (1, 1);
+insert into post_tag (post_id, tag_id) values (1, 2);
+insert into post_tag (post_id, tag_id) values (1, 3);
+insert into post_tag (post_id, tag_id) values (1, 4);
+insert into post_tag (post_id, tag_id) values (2, 1);
+insert into post_tag (post_id, tag_id) values (2, 2);
+insert into post_tag (post_id, tag_id) values (3, 1);
+insert into post_tag (post_id, tag_id) values (3, 4);
+insert into post_tag (post_id, tag_id) values (4, 1);
+insert into post_tag (post_id, tag_id) values (5, 2);
+insert into post_tag (post_id, tag_id) values (6, 1);
+
+
