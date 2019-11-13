bbaa99b18f7a00ad5e7af660a1d359ac6f9efb2c
Isaiah Brashears <isaiah.brashears13@gmail.com>
2019-11-13 15:39:40 -0600

    .

--- src/main/java/com/codeup/blog/blog/controllers/UserController.java
+++ src/main/java/com/codeup/blog/blog/controllers/UserController.java
@@ -20,29 +20,43 @@ public class UserController {
     }
 
     @GetMapping("/sign-up")
-    public String showSignupForm(Model model, @ModelAttribute String missing, @ModelAttribute String takenUsername ){
+    public String showSignupForm(Model model, @ModelAttribute String missing, @ModelAttribute String takenUsername, @ModelAttribute String takenEmail ){
         model.addAttribute("user", new User());
+
+        if (missing != null){
+            model.addAttribute("missing", missing);
+        }
+
+        if (takenEmail != null){
+            model.addAttribute("takenEmail", takenEmail);
+        }
 
         if (takenUsername != null){
             model.addAttribute("takenUsername", takenUsername);
         }
 
-        if (missing != null){
-            model.addAttribute("missing", missing);
-        }
         return "users/sign-up";
     }
 
     @PostMapping("/users/sign-up")
     public String saveUser(@ModelAttribute User user, Model vModel){
         if (userDao.findByUsername(user.getUsername()) != null){
-            vModel.addAttribute("takenUsername", "Username already taken chief. ");
+
+            vModel.addAttribute("takenUsername", "That Username is already taken, chief. ");
             return "/users/sign-up";
 
-        } else if (user.getEmail().isEmpty() || user.getPassword().isEmpty() || user.getUsername().isEmpty()){
-            vModel.addAttribute("missing", "Please fill out all forms." );
+        }else if(userDao.findByEmail(user.getEmail()) != null){
+
+            vModel.addAttribute("takenEmail", "That Email is already in use, champ. ");
             return "/users/sign-up";
+
+        }else if (user.getEmail().isEmpty() || user.getPassword().isEmpty() || user.getUsername().isEmpty()){
+
+            vModel.addAttribute("missing", "Please fill out all forms there, buddy." );
+            return "/users/sign-up";
+
         }else {
+
             String hash = passwordEncoder.encode(user.getPassword());
             user.setPassword(hash);
             userDao.save(user);
